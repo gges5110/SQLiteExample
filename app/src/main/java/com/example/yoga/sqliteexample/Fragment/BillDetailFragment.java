1e6896443df86017dfa1ef3b05d412e9be77a676
Yu-Chia Wu <gerrywu@utexas.edu>
2016-12-02 01:31:58 -0600

    Minor update
    Should be going in with the last commit

--- app/src/main/java/com/example/yoga/sqliteexample/Fragment/BillDetailFragment.java
+++ app/src/main/java/com/example/yoga/sqliteexample/Fragment/BillDetailFragment.java
@@ -4,19 +4,14 @@ package com.example.yoga.sqliteexample.Fragment;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.yoga.sqliteexample.MainPage;
 import com.example.yoga.sqliteexample.Model.Bill;
 import com.example.yoga.sqliteexample.Model.Person;
 import com.example.yoga.sqliteexample.R;
@@ -61,6 +56,7 @@ public class BillDetailFragment extends Fragment {
         Activity activity = getActivity();
         try {
             BillDetailFragmentInterface mListener = (BillDetailFragmentInterface) activity;
+            Log.d(TAG, "Bill Id = " + String.valueOf(bill_id));
             bill = mListener.getBill(bill_id);
             payer = mListener.getPerson(bill.getPayer());
         } catch (ClassCastException e) {
